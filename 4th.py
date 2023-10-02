2734ff5863c7a9c84a2e13dd4e063e788b44d580
AdnanShahid405 <adnanshahid405@gmail.com>
2023-10-02 14:07:12 +0500

    4th

---
+++ 4th.py
@@ -0,0 +1,9 @@
+import numpy as np
+A = np.array(([1,2,3,4,5]))
+print(f" The 1D array is : \n {A}")
+
+print(f"The type of array using type: {type(A)}")
+print(f"The type of array using dtype: {A.dtype}")
+print(f"The dimension of an array: {A.shape}")
+
+print(f"The size of an array: {A.size}")
